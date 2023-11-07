a05afd44bacdb138341cc25163969c75c3947db0
nikaholovach <57101059+nikaholovach@users.noreply.github.com>
2023-11-07 09:01:36 +0200

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const add = require("./add");
+const subtract = require("./subtract");
+const multiply = require("./multiply");
+const divide = require("./divide");
+
+module.exports = {
+  add,
+  subtract,
+  multiply,
+  divide,
+};
